dc5ee27ab9510c4198f8b96c5eb2a0e908c36019
Aaron Fishman <uk.afishman@gmail.com>
2021-07-22 16:08:57 +0100

    install bash script

---
+++ install.bash
@@ -0,0 +1,28 @@
+# Apt-get
+sudo apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    openjdk-11-jdk \
+    wget \
+    make \
+    git \
+    curl \
+    liblzma-dev \
+    libz-dev \
+    libncurses5-dev \
+    libncursesw5-dev \
+    libghc-bzlib-prof \
+    gcc \
+    unzip \
+    zlib1g-dev \
+    libncurses5-dev \
+    libbz2-dev \
+    liblzma-dev \
+    libcurl4-openssl-dev \
+    python3 \
+    python3-numpy \
+    python3-pip \
+    vim \
+    nano \
+    bc
+
+# Install the dependencies
+# sh install-fastqc.sh
