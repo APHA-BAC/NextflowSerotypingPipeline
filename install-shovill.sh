a13226b8a9883b185b36f39f458c44e183f50b8c
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-07-23 16:48:34 +0100

    Update install-shovill.sh

--- install-shovill.sh
+++ install-shovill.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.3-Linux-x86_64.sh
-sudo chmod +x Miniconda3-py37_4.8.3-Linux-x86_64.sh
+sudo chmod +x Miniconda3-py37_4.8.3-Linux-x86_64.sh -b
 ./Miniconda3-py37_4.8.3-Linux-x86_64.sh
 rm -rf Miniconda3-py37_4.8.3-Linux-x86_64.sh
 
