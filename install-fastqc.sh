188f0b9369b92d6a198d818586ff66a487713f7d
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-07-28 14:54:14 +0100

    Update install-fastqc.sh

--- install-fastqc.sh
+++ install-fastqc.sh
@@ -3,7 +3,7 @@
 sudo apt install unzip
 sudo apt install default-jre 
 
-wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/JaromirGuzinski/NextflowSerotypingPipeline/master/pipeline_component_software/FastQC/fastqc_v0.11.9.zip
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/APHA-BAC/NextflowSerotypingPipeline/master/pipeline_component_software/FastQC/fastqc_v0.11.9.zip
 unzip fastqc_v0.11.9.zip
 sudo cp -r FastQC /opt
 sudo chmod +x /opt/FastQC/fastqc
