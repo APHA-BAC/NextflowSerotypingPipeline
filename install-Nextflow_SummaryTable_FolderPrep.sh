96b2ef0491708ac01cf23f0b9b853880e2e0e652
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-07-29 16:49:12 +0100

    Update install-Nextflow_SummaryTable_FolderPrep.sh

--- install-Nextflow_SummaryTable_FolderPrep.sh
+++ install-Nextflow_SummaryTable_FolderPrep.sh
@@ -3,11 +3,11 @@
 mkdir /home/$USER/nextflow
 cd /home/$USER/nextflow
 curl -s https://get.nextflow.io | bash
-wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/JaromirGuzinski/NextflowSerotypingPipeline/master/SCE3_pipeline_update.nf
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/APHA-BAC/NextflowSerotypingPipeline/master/SCE3_pipeline_update.nf
 
 mkdir /home/$USER/summary
 cd /home/$USER/summary
-wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/JaromirGuzinski/NextflowSerotypingPipeline/master/summaryTable_Python3.py
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/APHA-BAC/NextflowSerotypingPipeline/master/summaryTable_Python3.py
 
 mkdir /home/$USER/WGS_Data
 mkdir /home/$USER/WGS_Results
