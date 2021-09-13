470460e580d95cbfa13d64303f62c9929a645f70
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-09-13 17:33:35 +0100

    Create inclusivity.bash

---
+++ test_isolates/jobs/inclusivity.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#================================================================
+# inclusivity.bash
+#================================================================
+#
+#% DESCRIPTION
+#%    Tests the nextflow pipeline on a minimal dataset
+
+# Import
+source tests/utils/aliases.bash
+
+# Args
+TESTCASE=$1
+
+serovar=$(print_csv_value './test_isolates/data/inclusivity_cases.csv' $TESTCASE serovar)
+accession=$(print_csv_value './test_isolates/data/inclusivity_cases.csv' $TESTCASE accession)
+
+# Fetch SRA Data
+prefetch $accession -O ./
+fasterq-dump ./$accession
+rm ./$accession/*.sra
+rm -r ./$accession
+
+gzip ${accession}_1.fastq ${accession}_2.fastq
+mv ${accession}_1.fastq.gz /reads/${accession}_R1.fastq.gz
+mv ${accession}_2.fastq.gz /reads/${accession}_R2.fastq.gz
+
+# Run nextflow
+nextflowtest
+
+# Check results
+WGS_CLUSTER_CSV=$(print_todays_wgs_cluster)
+assert_first_csv_row $WGS_CLUSTER_CSV "Outcome" "Pass"
+assert_first_csv_row $WGS_CLUSTER_CSV "serovar" "$serovar"
