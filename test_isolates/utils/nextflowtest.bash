5c42f782290befcd9af17c1d97c2c5c3951ab870
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-09-14 15:34:32 +0100

    Add files via upload

---
+++ test_isolates/utils/nextflowtest.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#================================================================
+# nextflowtest.bash
+#================================================================
+#
+#% DESCRIPTION
+#%    Integration tests for nextflow should be called this way. 
+
+nextflow run SCE3_pipeline_update.nf \
+--outdir "/WGS_Results/" \
+--reads "/WGS_Data/*_{R1,R2}.fastq.gz" \
+-with-report /artifacts/report.html
