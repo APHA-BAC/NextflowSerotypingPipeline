591bdd9b3f8a8eafe125be99cacacfebc3593f8f
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-09-14 11:05:36 +0100

    Create aliases.bash

---
+++ test_isolates/utils/aliases.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#================================================================
+# aliases.bash
+#================================================================
+#
+#% DESCRIPTION
+#%    A number of aliases useful for testing
+shopt -s expand_aliases
+
+alias nextflowtest="bash test_isolates/utils/nextflowtest.bash"
+
+alias print_todays_wgs_cluster="sh test_isolates/utils/print_todays_wgs_cluster.sh"
+
+alias assert_first_csv_row="python test_isolates/utils/assert_first_csv_row.py"
+
+alias combine_fastq="python test_isolates/utils/combine_fastq.py"
+
+alias print_csv_value="python test_isolates/utils/print_csv_value.py"
