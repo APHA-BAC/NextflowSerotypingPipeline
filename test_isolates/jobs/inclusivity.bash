94b21e678b2d7ea20e85eb4335d1caaa5544f752
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-09-17 09:46:23 +0100

    Update inclusivity.bash

--- test_isolates/jobs/inclusivity.bash
+++ test_isolates/jobs/inclusivity.bash
@@ -13,8 +13,8 @@ source /NextflowSerotypingPipeline/test_isolates/utils/aliases.bash
 # Args
 TESTCASE=$1
 
-serovar=$(print_csv_value '/NextflowSerotypingPipeline/test_isolates/data/inclusivity_cases.csv' $TESTCASE serovar)
-accession=$(print_csv_value '/NextflowSerotypingPipeline/test_isolates/data/inclusivity_cases.csv' $TESTCASE accession)
+serovar=$(print_csv_value './NextflowSerotypingPipeline/test_isolates/data/inclusivity_cases.csv' $TESTCASE serovar)
+accession=$(print_csv_value './NextflowSerotypingPipeline/test_isolates/data/inclusivity_cases.csv' $TESTCASE accession)
 
 # Fetch SRA Data
 prefetch $accession -O ./
