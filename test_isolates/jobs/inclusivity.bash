7a53b11d37416a8eb29440fb363fec6a8fa153fc
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-09-21 15:16:36 +0100

    Update inclusivity.bash

--- test_isolates/jobs/inclusivity.bash
+++ test_isolates/jobs/inclusivity.bash
@@ -31,105 +31,105 @@ nextflowtest
 
 
 #check dir contents
-for file in /NextflowSerotypingPipeline/WGS_Data/*; do
-  echo "${file##*/}"
-done
+#for file in /NextflowSerotypingPipeline/WGS_Data/*; do
+#  echo "${file##*/}"
+#done
 #*
 
-for file in /NextflowSerotypingPipeline/WGS_Results/*; do
-  echo "${file##*/}"
-done
+#for file in /NextflowSerotypingPipeline/WGS_Results/*; do
+#  echo "${file##*/}"
+#done
 #*
 
-for entry in "/NextflowSerotypingPipeline/WGS_Results"/*
-do
-  echo "$entry"
-done
+#for entry in "/NextflowSerotypingPipeline/WGS_Results"/*
+#do
+#  echo "$entry"
+#done
 #/NextflowSerotypingPipeline/WGS_Results/*
 
-for file in /WGS_Data/*; do
-  echo "${file##*/}"
-done
+#for file in /WGS_Data/*; do
+#  echo "${file##*/}"
+#done
 #ERR2235_R1...fastq.gz
 #ERR2235_R2...fastq.gz
 
-for file in /WGS_Results/*; do
-  echo "${file##*/}"
-done
+#for file in /WGS_Results/*; do
+#  echo "${file##*/}"
+#done
 #*
 
-for entry in "/WGS_Results"/*
-do
-  echo "$entry"
-done
+#for entry in "/WGS_Results"/*
+#do
+#  echo "$entry"
+#done
 #/WGS_Results/*
 
-for file in /WGS_Data/test_isolates/*; do
-  echo "${file##*/}"
-done
+#for file in /WGS_Data/test_isolates/*; do
+#  echo "${file##*/}"
+#done
 #*
 
-for file in /WGS_Results/test_isolates/*; do
-  echo "${file##*/}"
-done
+#for file in /WGS_Results/test_isolates/*; do
+#  echo "${file##*/}"
+#done
 #*
 
-for entry in "/WGS_Results/test_isolates"/*
-do
-  echo "$entry"
-done
+#for entry in "/WGS_Results/test_isolates"/*
+#do
+#  echo "$entry"
+#done
 #/WGS_Results/test_isolates/*
 
 
-for file in /home/WGS_Data/*; do
-  echo "${file##*/}"
-done
+#for file in /home/WGS_Data/*; do
+#  echo "${file##*/}"
+#done
 #test_isolates
 
-for file in /home/WGS_Results/*; do
-  echo "${file##*/}"
-done
+#for file in /home/WGS_Results/*; do
+#  echo "${file##*/}"
+#done
 #test_isolates
 
-for entry in "/home/WGS_Results"/*
-do
-  echo "$entry"
-done
+#for entry in "/home/WGS_Results"/*
+#do
+#  echo "$entry"
+#done
 #/home/WGS_Results/test_isolates
 
 
-for file in /home/$USER/WGS_Data/*; do
-  echo "${file##*/}"
-done
+#for file in /home/$USER/WGS_Data/*; do
+#  echo "${file##*/}"
+#done
 
-for file in /home/$USER/WGS_Results/*; do
-  echo "${file##*/}"
-done
+#for file in /home/$USER/WGS_Results/*; do
+#  echo "${file##*/}"
+#done
 
-for entry in "/home/$USER/WGS_Results"/*
-do
-  echo "$entry"
-done
-
-
-for file in /home/$USER/WGS_Data/test_isolates*; do
-  echo "${file##*/}"
-done
-
-for file in /home/$USER/WGS_Results/test_isolates*; do
-  echo "${file##*/}"
-done
-
-for entry in "/home/$USER/WGS_Results/test_isolates"/*
-do
-  echo "$entry"
-done
+#for entry in "/home/$USER/WGS_Results"/*
+#do
+#  echo "$entry"
+#done
 
 
-for entry in "/home/WGS_Results/test_isolates"/*
-do
-  echo "$entry"
-done
+#for file in /home/$USER/WGS_Data/test_isolates*; do
+#  echo "${file##*/}"
+#done
+
+#for file in /home/$USER/WGS_Results/test_isolates*; do
+#  echo "${file##*/}"
+#done
+
+#for entry in "/home/$USER/WGS_Results/test_isolates"/*
+#do
+#  echo "$entry"
+#done
+
+
+#for entry in "/home/WGS_Results/test_isolates"/*
+#do
+#  echo "$entry"
+#done
 
 
 for entry in "/home/WGS_Results/test_isolates/ERR2230776"/*
@@ -152,26 +152,25 @@ do
   echo "$entry"
 done
 
-find  /home/ -name '*SummaryTable*'
-find  /home/ -name '*.csv*'
+#find  /home/ -name '*SummaryTable*'
+#find  /home/ -name '*.csv*'
 
-find  /WGS_Results/ -name '*SummaryTable*'
-find  /NextflowSerotypingPipeline/ -name '*SummaryTable*'
-find  /test_isolates/ -name '*SummaryTable*'
-find  /NextflowSerotypingPipeline/test_isolates/ -name '*SummaryTable*'
-find  /NextflowSerotypingPipeline/WGS_Results/ -name '*SummaryTable*'
-find  /NextflowSerotypingPipeline/test_isolates/ -name '*Summary*'
-find  /NextflowSerotypingPipeline/test_isolates/ -name '*Summary*'
+#find  /WGS_Results/ -name '*SummaryTable*'
+#find  /NextflowSerotypingPipeline/ -name '*SummaryTable*'
+#find  /test_isolates/ -name '*SummaryTable*'
+#find  /NextflowSerotypingPipeline/test_isolates/ -name '*SummaryTable*'
+#find  /NextflowSerotypingPipeline/WGS_Results/ -name '*SummaryTable*'
+#find  /NextflowSerotypingPipeline/test_isolates/ -name '*Summary*'
+#find  /NextflowSerotypingPipeline/test_isolates/ -name '*Summary*'
 
-find  /NextflowSerotypingPipeline/WGS_Results/ -name '*Table*'
-find  /NextflowSerotypingPipeline/WGS_Results/ -name '*Summary*'
-find  /NextflowSerotypingPipeline/WGS_Results/ -name '*.csv*'
+#find  /NextflowSerotypingPipeline/WGS_Results/ -name '*Table*'
+#find  /NextflowSerotypingPipeline/WGS_Results/ -name '*Summary*'
+#find  /NextflowSerotypingPipeline/WGS_Results/ -name '*.csv*'
 
-find  /NextflowSerotypingPipeline/test_isolates/ -name '*Summary*'
-
-
+#find  /NextflowSerotypingPipeline/test_isolates/ -name '*Summary*'
 #check dir contents
 
+python /NextflowSerotypingPipeline/summaryTable_Python3.py 
 
 # Check results
 WGS_CLUSTER_CSV=$(print_todays_wgs_cluster)
