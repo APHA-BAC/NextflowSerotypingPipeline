ee02be1773e9386320401ce050c6e798c9f854f9
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-10-05 17:25:51 +0100

    Update nextflowtest.bash

--- test_isolates/utils/nextflowtest.bash
+++ test_isolates/utils/nextflowtest.bash
@@ -8,6 +8,6 @@
 #%    Integration tests for nextflow should be called this way. 
 
 /NextflowSerotypingPipeline/nextflow run SCE3_pipeline_update.nf \
---outdir "/WGS_Results/" \
+--outdir "/home/WGS_Results/TestIsolates/" \
 --reads "/home/WGS_Data/TestIsolates/*_{R1,R2}.fastq.gz" \
 -with-report /artifacts/report.html
