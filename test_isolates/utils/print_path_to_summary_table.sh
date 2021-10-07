361cbadced1c5866807685c168bdd0805931d678
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-10-07 12:32:20 +0100

    Add files via upload

---
+++ test_isolates/utils/print_path_to_summary_table.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+#================================================================
+# print_path_to_summary_table.sh
+#================================================================
+#
+#% DESCRIPTION
+#%    Prints the filepath of the Nextflow run Summary Table CSV. 
+
+echo /home/WGS_Results/TestIsolates/TestIsolates_SummaryTable.csv
