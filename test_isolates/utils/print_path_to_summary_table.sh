97090c8924bd93525a3088b36834d9fab423b612
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2021-10-19 16:17:15 +0100

    Update print_path_to_summary_table.sh

--- test_isolates/utils/print_path_to_summary_table.sh
+++ test_isolates/utils/print_path_to_summary_table.sh
@@ -4,6 +4,7 @@
 # print_path_to_summary_table.sh
 #================================================================
 #
+#
 #% DESCRIPTION
 #%    Prints the filepath of the Nextflow run Summary Table CSV. 
 
