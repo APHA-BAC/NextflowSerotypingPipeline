fb179c19be3b604624b95112d10d67de0ea8aa4a
Aaron Fishman <aaron.fishman@apha.gov.uk>
2021-10-29 16:52:34 +0100

    symlinking nextflow

--- install-nextflow.sh
+++ install-nextflow.sh
@@ -6,3 +6,5 @@ mkdir $HOME/nextflow
 cd $HOME/nextflow
 curl -L0 https://github.com/nextflow-io/nextflow/releases/download/v21.04.1/nextflow-21.04.1-all | bash
 # This ^ gives a warning, "curl: (23) Failed writing body (4096 != 16384)", but seems to work?
+
+ln -s $PWD/nextflow /usr/local/bin/nextflow
