8705fbc8bfa88073792fffa5e9cceeb183ea2bd1
Aaron Fishman <aaron.fishman@apha.gov.uk>
2021-10-29 15:25:56 +0100

    installing conda

---
+++ install-conda.sh
@@ -0,0 +1,22 @@
+#########################################################################################
+# Miniconda
+cd $HOME
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.3-Linux-x86_64.sh
+sudo chmod +x Miniconda3-py37_4.8.3-Linux-x86_64.sh
+sudo ./Miniconda3-py37_4.8.3-Linux-x86_64.sh -b -p /opt/conda
+# -b flag is for silent batch installation with no direct PATH modification
+rm Miniconda3-py37_4.8.3-Linux-x86_64.sh
+
+export PATH=/opt/conda/bin:$PATH
+/opt/conda/bin/conda init bash
+source ~/.bashrc
+
+# Conda environment
+# Now this is where things change. I create a specific Conda environment for the pipeline
+# Any other projects on the SCE3 machine will thus be unaffected by conda and pip installs within this environment
+conda create -n salmpipe python=3.7
+conda activate salmpipe
+
+# Matplotlib
+pip install matplotlib
