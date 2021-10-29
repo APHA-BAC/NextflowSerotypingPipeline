bfe76a29ff4cc0232389a68fbdec4c7540d3f678
Aaron Fishman <aaron.fishman@apha.gov.uk>
2021-10-29 15:32:10 +0100

    installing shovill

--- install-shovill.sh
+++ install-shovill.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-
-wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.3-Linux-x86_64.sh
-sudo chmod +x Miniconda3-py37_4.8.3-Linux-x86_64.sh 
-./Miniconda3-py37_4.8.3-Linux-x86_64.sh -b
-rm -rf Miniconda3-py37_4.8.3-Linux-x86_64.sh
-
+###########################################################################################
+# Shovill
 conda install -c conda-forge -c bioconda -c defaults shovill=0.9.0
-
-
+shovill --check
