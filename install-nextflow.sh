de70389d5ea973c3658d16e3d8933eb51018d55f
Joshua Potter <joshuapotter@ranch-210.int.sce.network>
2021-11-04 11:35:11 +0000

    still playing with paths...

--- install-nextflow.sh
+++ install-nextflow.sh
@@ -7,6 +7,6 @@ cd $HOME/nextflow
 curl -L0 https://github.com/nextflow-io/nextflow/releases/download/v21.04.1/nextflow-21.04.1-all | bash
 # This ^ gives a warning, "curl: (23) Failed writing body (4096 != 16384)", but seems to work?
 
-ln -s $PWD/nextflow /usr/local/bin/nextflow
+ln -s $HOME/nextflow /usr/local/bin/nextflow
 
 fetcher --url="https://github.com/APHA-BAC/NextflowSerotypingPipeline/blob/master/SCE3_pipeline_update.nf" --out=$HOME/nextflow
--- test_isolates/utils/nextflowtest.bash
+++ test_isolates/utils/nextflowtest.bash
@@ -7,7 +7,7 @@
 #% DESCRIPTION
 #%    Integration tests for nextflow should be called this way. 
 
-$HOME/nextflow/nextflow run $HOME/nextflow/SCE3_pipeline_update.nf \
+nextflow run $HOME/nextflow/SCE3_pipeline_update.nf \
 --outdir "$HOME/WGS_Results/TestIsolates/" \
 --reads "$HOME/WGS_Data/TestIsolates/*_{R1,R2}.fastq.gz" \
 -with-report /artifacts/report.html
