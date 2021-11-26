aa35e1208a7380e3dd48b1e8fd918fd1b6f782d6
Joshua Potter <jhtpotter@gmail.com>
2021-11-26 14:12:09 +0000

    changed install to fixed version

--- install-fastp.sh
+++ install-fastp.sh
@@ -5,7 +5,8 @@ set -e
 # fastp
 cd $HOME
 
-wget --no-check-certificate --content-disposition http://opengene.org/fastp/fastp
+wget --no-check-certificate --content-disposition http://opengene.org/fastp/fastp.0.23.1
+mv fastp.0.23.1 fastp
 sudo cp fastp /opt
 sudo chmod +x /opt/fastp
 sudo ln -s /opt/fastp /usr/local/bin
