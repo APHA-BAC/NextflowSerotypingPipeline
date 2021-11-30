9cc80349aacfe150d49ecd88a9db0465ca8bf87c
Joshua Potter <jhtpotter@gmail.com>
2021-11-30 16:32:26 +0000

    removed the need to get current nextflow script with fetcher

--- install-nextflow.sh
+++ install-nextflow.sh
@@ -7,7 +7,6 @@ cd $HOME/nextflow
 curl -L0 https://github.com/nextflow-io/nextflow/releases/download/v21.04.1/nextflow-21.04.1-all | bash
 # This ^ gives a warning, "curl: (23) Failed writing body (4096 != 16384)", but seems to work?
 
-ln -s $HOME/nextflow /usr/local/bin/nextflow
+sudo ln -s $HOME/nextflow/nextflow /usr/local/bin
 
-fetcher --url="https://github.com/APHA-BAC/NextflowSerotypingPipeline/blob/seqtk_subsampling/SCE3_pipeline_update.nf" --out=$HOME/nextflow
 # fetcher --url="https://github.com/APHA-BAC/NextflowSerotypingPipeline/blob/master/SCE3_pipeline_update.nf" --out=$HOME/nextflow
