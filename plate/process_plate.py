bcbb949c65709788e4cd2927f9a0a334afaf84c3
Aaron Fishman <uk.afishman@gmail.com>
2021-12-02 13:13:36 +0000

    recursive s3 download

--- plate/process_plate.py
+++ plate/process_plate.py
@@ -1,14 +1,18 @@
 import subprocess
 
+
+RESULTS_DIRECTORY = '/home/aaronfishman/temp/salmonella-results/'
+READS_DIRECTORY = "/home/aaronfishman/temp/salmonella-reads/" 
+
 # TODO: get plate name from Josh's cpde
 def run_pipeline(reads, results, plate_name):
     subprocess.run([
-    "sudo", "docker", "run", "-it", 
-    "-v", f"{reads}:/root/WGS_Data/{plate_name}/",
-    "-v", f"{results}:/root/WGS_Results/{plate_name}/", 
-    "jguzinski/salmonella-seq:prod",
-    "/root/nextflow/nextflow", "SCE3_pipeline_update.nf",
-    "--runID", plate_name
+        "sudo", "docker", "run", "-it", 
+        "-v", f"{reads}:/root/WGS_Data/{plate_name}/",
+        "-v", f"{results}:/root/WGS_Results/{plate_name}/", 
+        "jguzinski/salmonella-seq:prod",
+        "/root/nextflow/nextflow", "SCE3_pipeline_update.nf",
+        "--runID", plate_name
     ])
 
 
@@ -23,22 +27,46 @@ def rename_WGS(readFiles, homeWGSDir):
         os.rename("{}/{}".format(homeWGSDir, readFile), "{}/{}".format(homeWGSDir, newName))
     print("\n\n")
 
+def download_s3(s3_uri, destination):
+    subprocess.run(["aws", "s3", "cp", "--recursive",
+        s3_uri,
+        destination
+    ])
+
+    # TODO: throw exception if it fails
+
+
+def s3_uri_to_plate_name(s3_uri):
+    return ""
+
 if __name__ == '__main__':
-    results_directoy = '/home/aaronfishman/temp/salmonella-results/'
-    reads_directory = "/home/aaronfishman/temp/salmonella-reads/"
+    # TODO: Use Josh's interactive plate selection code
+
+    s3_uri = 's3://s3-csu-001/temp/new-plate/'
+
+    plate_name = <date ddmmyy>_APHA_<plate_name>
+
+    quit()
+
+    download_s3(s3_uri, )
 
 
     # Download raw reads from S3 --> EC2
-    plate_name = "new-plate"
-    plate_reads_dir = reads_directory + plate_name + '/'
 
+   
+    plate_name = "new-plate"
+
+    # Run plate
+    plate_reads_dir = READS_DIRECTORY + plate_name + '/'
 
     # TODO: Backup raw reads from EC2 --> FSX
     # Integrity checks should be done, Josh's script already does this
 
-    plate_results_dir = results_directoy + plate_name + '/'
+    plate_results_dir = RESULTS_DIRECTORY + plate_name + '/'
 
-    print("plate_reads_dir", plate_reads_dir)
-    print("plate_results_dir", plate_results_dir)
+    # Run the pipeline
 
     run_pipeline(plate_reads_dir, plate_results_dir, plate_name)
+
+    # TODO: Backup to fsx
+    
