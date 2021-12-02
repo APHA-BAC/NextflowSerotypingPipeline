81e0465cc3b16b631499226fd9b84aa14862c86e
Aaron Fishman <uk.afishman@gmail.com>
2021-12-02 12:52:28 +0000

    running docker from python

--- plate/process_plate.py
+++ plate/process_plate.py
@@ -1,25 +1,15 @@
-
-
-results_directoy = '/home/aaronfishman/temp/salmonella-results/'
-reads_directory = "/home/aaronfishman/temp/salmonella-reads/"
-
-
-# Download raw reads from S3 --> EC2
-plate_name = "new-plate"
-plate_reads_dir = reads_directory + plate_name + '/'
-
-
-# TODO: Backup raw reads from EC2 --> FSX
-# Integrity checks should be done, Josh's script already does this
-
-plate_results_dir = results_directoy + plate_name + '/'
-
-print("plate_reads_dir", plate_reads_dir)
-print("plate_results_dir", plate_results_dir)
-
+import subprocess
 
 # TODO: get plate name from Josh's cpde
-
+def run_pipeline(reads, results, plate_name):
+    subprocess.run([
+    "sudo", "docker", "run", "-it", 
+    "-v", f"{reads}:/root/WGS_Data/{plate_name}/",
+    "-v", f"{results}:/root/WGS_Results/{plate_name}/", 
+    "jguzinski/salmonella-seq:prod",
+    "/root/nextflow/nextflow", "SCE3_pipeline_update.nf",
+    "--runID", plate_name
+    ])
 
 
 
@@ -33,5 +23,22 @@ def rename_WGS(readFiles, homeWGSDir):
         os.rename("{}/{}".format(homeWGSDir, readFile), "{}/{}".format(homeWGSDir, newName))
     print("\n\n")
 
+if __name__ == '__main__':
+    results_directoy = '/home/aaronfishman/temp/salmonella-results/'
+    reads_directory = "/home/aaronfishman/temp/salmonella-reads/"
 
 
+    # Download raw reads from S3 --> EC2
+    plate_name = "new-plate"
+    plate_reads_dir = reads_directory + plate_name + '/'
+
+
+    # TODO: Backup raw reads from EC2 --> FSX
+    # Integrity checks should be done, Josh's script already does this
+
+    plate_results_dir = results_directoy + plate_name + '/'
+
+    print("plate_reads_dir", plate_reads_dir)
+    print("plate_results_dir", plate_results_dir)
+
+    run_pipeline(plate_reads_dir, plate_results_dir, plate_name)
