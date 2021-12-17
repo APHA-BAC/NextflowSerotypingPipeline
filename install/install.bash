491a20ebbed3531760bf4927bada91adc52952f7
Aaron Fishman <uk.afishman@gmail.com>
2021-12-17 11:02:36 +0000

    moving installers into ./install/

--- install/install.bash
+++ install/install.bash
@@ -27,46 +27,19 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y -q \
     python3
 
 
-cp ./install-conda.sh ./install/install-conda.sh
 bash -e install/install-conda.sh
-
-cp ./install-fastp.sh ./install
-bash -e ./install/install-fastp.sh
-
-cp ./install-fastqc.sh ./install/install-fastqc.sh
+bash -e install/install-fastp.sh
 bash -e install/install-fastqc.sh
-
-cp ./install-seqtk.sh ./install/install-seqtk.sh
 bash -e install/install-seqtk.sh
-
-cp ./install-shovill.sh ./install/install-shovill.sh
 bash -e install/install-shovill.sh
-
-cp ./install-quast.sh ./install/install-quast.sh
 bash -e install/install-quast.sh
-
-cp ./install-most.sh ./install/install-most.sh
 bash -e install/install-most.sh
-
-cp ./install-kmerid.sh ./install/install-kmerid.sh
 bash -e install/install-kmerid.sh
-
-cp ./install-seqsero2.sh ./install/install-seqsero2.sh
 bash -e install/install-seqsero2.sh
-
-cp ./install-sistr.sh ./install/install-sistr.sh
 bash -e install/install-sistr.sh
-
-cp ./install-srst2.sh ./install/install-srst2.sh
 bash -e install/install-srst2.sh
-
-cp ./install-nextflow.sh ./install/install-nextflow.sh
 bash -e install/install-nextflow.sh
-
-cp ./install-sra-toolkit.sh ./install/install-sra-toolkit.sh
 bash -e install/install-sra-toolkit.sh
-
-cp ./setup-environment.sh ./install/setup-environment.sh
 bash -e install/setup-environment.sh
 
 
