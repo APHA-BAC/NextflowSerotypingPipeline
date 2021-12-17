b633610448d1f3ba2d4a23b684b52bd44b4e867b
Aaron Fishman <uk.afishman@gmail.com>
2021-12-17 10:47:26 +0000

    consolidating apt-get installs

--- install/install.bash
+++ install/install.bash
@@ -3,32 +3,29 @@ set -e
 # TODO: DRY up the apt-get so it's in one call
 
 apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q sudo
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q apt-utils
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q make
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q gcc
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q bc
+DEBIAN_FRONTEND=noninteractive apt-get install -y -q \
+    sudo \
+    apt-utils \
+    make \
+    gcc \
+    bc \
+    git \
+    wget \
+    curl \
+    libz-dev \
+    libbz2-dev \
+    libncurses5-dev \
+    libncursesw5-dev \
+    libghc-bzlib-prof \
+    zlib1g-dev \
+    libcurl4-openssl-dev \
+    libfile-copy-recursive-perl \
+    libio-socket-ssl-perl \
+    libio-tee-perl \
+    libunicode-string-perl \
+    nano \
+    python3
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q git
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q wget
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q curl
-
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q libz-dev
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q libbz2-dev
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q libncurses5-dev
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q libncursesw5-dev
-
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q libghc-bzlib-prof
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q zlib1g-dev
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q libcurl4-openssl-dev
-
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q libfile-copy-recursive-perl
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q libio-socket-ssl-perl
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q libio-tee-perl
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q libunicode-string-perl
-
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q nano
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q python3
 
 cp ./install-fastp.sh ./install
 bash -e ./install/install-fastp.sh
@@ -36,8 +33,8 @@ bash -e ./install/install-fastp.sh
 cp ./install-fastqc.sh ./install/install-fastqc.sh
 bash -e install/install-fastqc.sh
 
-#export PATH=$PATH:/opt/conda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 PATH=/opt/conda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 cp ./install-conda.sh ./install/install-conda.sh
 bash -e install/install-conda.sh
 
