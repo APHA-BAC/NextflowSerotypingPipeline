c4dd0b4e17e6612c0db51e6f37d3e7a0e4eab812
Jaromir Guzinski <71131038+JaromirGuzinski@users.noreply.github.com>
2022-01-11 17:27:25 +0000

    Update install-most.sh

--- install/install-most.sh
+++ install/install-most.sh
@@ -9,7 +9,7 @@ pip install lxml==4.5.2
 pip install biopython==1.73
 
 # TODO: download directly from authour
-wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/APHA-BAC/NextflowSerotypingPipeline/master/pipeline_component_software/most/most.zip
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/APHA-BAC/MOST-PHE/tree/main/pipeline_component_software/most/most.zip
 unzip most.zip
 sudo cp -r most /opt
 sudo chmod +x /opt/most/MOST-master/MOST.py
