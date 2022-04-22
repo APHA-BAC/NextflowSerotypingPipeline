0223c089c2f7d30b8ad24b982db5c22b0cd85b75
Arslan Hussaini <bobthejoebob96@gmail.com>
2022-04-22 11:04:48 +0100

    Minor changes and fixes

--- install/setup-environment.sh
+++ install/setup-environment.sh
@@ -8,6 +8,5 @@ mkdir $HOME/WGS_Results
 #################################################################################################
 # Summary table script
 mkdir $HOME/summary # Changed to $HOME for SC3 - We don't want summary installed in /home/summary
-cd $HOME/summary
 
-cp ~/NextflowSerotypingPipeline/summaryTable_reworked.py ./
+cp ./summaryTable_reworked.py $HOME/summary
