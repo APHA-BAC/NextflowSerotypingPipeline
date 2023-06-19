0ddfb835f6a53e2cb325396cc32d502a8880cf4d
Arslan Hussaini <arslanhussaini@ranch-210.int.sce.network>
2023-06-19 16:04:13 +0100

    Fixed test scripts referring to WGS_Data and WGS_Results and changed it to wgs-reads and wgs-results

--- install/install.bash
+++ install/install.bash
@@ -45,4 +45,4 @@ pip install awscli
 pip install boto3
 
 # Output folder for running jobs
-mkdir $HOME/WGS_Data/TestIsolates/ $HOME/WGS_Results/TestIsolates/ 
+mkdir $HOME/wgs-reads/TestIsolates/ $HOME/wgs-results/TestIsolates/ 
