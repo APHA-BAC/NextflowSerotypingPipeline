9fa2d08f7c656e2ab2b96655950d48642b178f84
Arslan Hussaini <arslanhussaini@ranch-210.int.sce.network>
2023-11-17 15:28:21 +0000

    Updated validation 250 tables and adjusted prod_validate to also compare LIMS_rules

--- plate/prod_validate.py
+++ plate/prod_validate.py
@@ -6,7 +6,7 @@ import argparse
 DEFAULT_IMAGE = "jguzinski/salmonella-seq:master"
 DEFAULT_READS_DIR = os.path.expanduser('~/mnt/Salmonella/BAC3_NGS_Archive/Salmonella/validation_panel_Feb2022/')
 DEFAULT_RESULTS_DIR = os.path.expanduser('~/wgs-results/validation_test/')
-DEFAULT_EXPECTED_CSV_PATH = '../validation250/validation250_Feb2022_EXPECTED_SummaryTable.csv'
+DEFAULT_EXPECTED_CSV_PATH = '../validation250/validation250_Feb2022_EXPECTED_SummaryTable_plusLIMS.csv'
 DEFAULT_OUTCOME_PATH = './outcome.csv'
 
 def load_summary_table(csv_path):
@@ -30,12 +30,12 @@ def load_summary_table(csv_path):
 def analyse_results(expected_csv_path, actual_csv_path):
     """ Returns a merged DataFrame containing an Outcome column that indicates consistency """
     # Load
-    expected_df = load_summary_table(expected_csv_path)[["Isolate_ID", "Consensus"]]
+    expected_df = load_summary_table(expected_csv_path)[["Isolate_ID", "Consensus", "LIMS_Status", "LIMS_Reason", "MOST_Light"]]
     actual_df = load_summary_table(actual_csv_path)
 
     # Rename Columns
-    expected_df = expected_df.rename(columns={"Consensus": "ExpectedConsensus"})
-    actual_df = actual_df.rename(columns={"Consensus": "ActualConsensus"})
+    expected_df = expected_df.rename(columns={"Consensus": "ExpectedConsensus", "LIMS_Status": "ExpectedStatus", "LIMS_Reason": "ExpectedReason", "MOST_Light": "ExpectedLight"})
+    actual_df = actual_df.rename(columns={"Consensus": "ActualConsensus", "LIMS_Status": "ActualStatus", "LIMS_Reason": "ActualReason", "MOST_Light": "ActualLight"})
 
     # Join
     merged = expected_df.merge(actual_df, on='Isolate_ID', how='left')
@@ -45,6 +45,11 @@ def analyse_results(expected_csv_path, actual_csv_path):
     merged.loc[merged['ActualConsensus']==merged['ExpectedConsensus'], 'Outcome'] = 'success'
     merged.loc[merged['ActualConsensus']!=merged['ExpectedConsensus'], 'Outcome'] = 'fail'
     merged.loc[merged['ActualConsensus'].isna(), 'success'] = 'missing'
+    
+    merged.loc[merged['ActualStatus']==merged['ExpectedStatus'], 'Outcome'] = 'success'
+    merged.loc[merged['ActualStatus']!=merged['ExpectedStatus'], 'Outcome'] = 'fail'
+    merged.loc[merged['ActualStatus'].isna(), 'success'] = 'missing'
+    
 
     # Outcome
     return merged
@@ -56,13 +61,13 @@ def validate(
     outcome_csv_path,
     image
 ):
-    if os.path.exists(results_path):
-        raise Exception("Results path already exists; Path must not exist: ", results_path)
+    # if os.path.exists(results_path):
+    #     raise Exception("Results path already exists; Path must not exist: ", results_path)
 
-    os.makedirs(results_path)
+    # os.makedirs(results_path)
 
-    process_plate.run_pipeline(reads_path, results_path, "validation_test", image)
-    actual_csv_path = results_path + "/validation_test_SummaryTable.csv"
+    # process_plate.run_pipeline(reads_path, results_path, "validation_test", image)
+    actual_csv_path = results_path + "/validation_test_SummaryTable_plusLIMS.csv"
 
     merged = analyse_results(expected_csv_path, actual_csv_path)
 
