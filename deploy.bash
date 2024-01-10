ad12cef93b11e88a09ebd57c202e13c7e1195466
Nick Pestell <nicholas.pestell@apha.gov.uk>
2024-01-10 17:10:23 +0000

    fix bug

--- deploy.bash
+++ deploy.bash
@@ -1,4 +1,4 @@
 docker pull jguzinski/salmonella-seq:prod
 docker tag jguzinski/salmonella-seq:prod 714385292749.dkr.ecr.eu-west-1.amazonaws.com/salmonella-ec2-0-1-1:latest
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin :wq714385292749.dkr.ecr.eu-west-1.amazonaws.com
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 714385292749.dkr.ecr.eu-west-1.amazonaws.com
 docker push 714385292749.dkr.ecr.eu-west-1.amazonaws.com/salmonella-ec2-0-1-1:latest
