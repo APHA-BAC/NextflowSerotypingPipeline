b11f1c160597c70699d74e0781274feb1672e1ab
Arslan Hussaini <arslanhussaini@ranch-310.int.sce.network>
2024-01-22 13:45:55 +0000

    Fixed master summary table script so it also adds date and plate_id for new summary tables

--- plate/update_master_sum.py
+++ plate/update_master_sum.py
@@ -1,10 +1,25 @@
 import pandas as pd
+import re
 
 
-def update_summary(master_sum, new_sum):
-	master_df = pd.read_csv(master_sum)
-	new_df = pd.read_csv(new_sum)
+def update_summary(master_sum,new_sum):
+	master_df=pd.read_csv(master_sum)
+	new_df=pd.read_csv(new_sum)
+	sum_info=re.search("(\d{6})_APHA_(.*_\d{4})",str(new_sum))
+	date=sum_info.group(1)
+	plate_id=sum_info.group(2)
 
-	master_df = pd.concat([master_df, new_df])
-	master_df.to_csv(master_sum, index=False)
+	day=date[0:2]
+	month=date[2:4]
+	year=date[4:6]
+	date=day+"/"+month+"/"+year
 
+	new_df.loc[0:,'Date']=date
+	new_df.loc[0:,'Plate_ID']=plate_id
+
+	master_df=pd.concat([master_df,new_df])
+	master_df.to_csv(master_sum,index=False)
+
+
+
+update_summary("./master_summary.csv","170124_APHA_NB552234_0302_SummaryTable_plusLIMS.csv")
