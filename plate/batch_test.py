04dd0ffca18546fb0873ab27a7b7a4d04b5cb1cf
Arslan Hussaini <arslanhussaini@ranch-210.int.sce.network>
2024-11-06 11:30:03 +0000

    Changed formate of success and failure email

--- plate/batch_test.py
+++ plate/batch_test.py
@@ -191,7 +191,7 @@ def run_plate(reads_uri, reads_dir, results_uri, kmer_uri):
     try:
         run_pipeline(plate_name, record_output=True)
         with open("{}_finished.scemail".format(plate_name), 'w') as text_file:
-            text_file.write("Plate {} run has completed.".format(plate_name))
+            text_file.write("The following plate has completed:\n{}".format(plate_name))
         run(['aws', 's3', 'cp', './{}_finished.scemail'.format(plate_name), 's3://s3-scemail-3cfh-salmonella-serotyping-pipeline-1-0-0/salmonella-serotyping-pipeline/request/', 
             '--acl', 'bucket-owner-full-control'])
         # Upload results to s3
@@ -215,7 +215,7 @@ def run_plate(reads_uri, reads_dir, results_uri, kmer_uri):
 
     except:
         with open("{}_failed.scemail".format(plate_name), 'w') as text_file:
-            text_file.write("This run has failed. Please see log file for more details")
+            text_file.write("The following plate has failed: \n{} \nPlease see log file for more details".format(plate_name))
         time.sleep(10)
         run(['aws', 's3', 'cp', './{}_failed.scemail'.format(plate_name), 's3://s3-scemail-3cfh-salmonella-serotyping-pipeline-1-0-0/salmonella-serotyping-pipeline/request/', 
             '--acl', 'bucket-owner-full-control'])
